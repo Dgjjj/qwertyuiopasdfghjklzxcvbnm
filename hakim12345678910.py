b5483b9e46e75009ec922f7f0bd1811d2a3fcac1
hakim <151940057+Dgjjj@users.noreply.github.com>
2023-12-01 16:16:12 +0100

    Add files via upload

---
+++ hakim12345678910.py
@@ -0,0 +1,14 @@
+#-*-coding:utf8;-*-
+#qpy:console
+
+#import pyautogui as gui
+#import time
+#import random,string
+
+from getpass import getqass
+
+user=input("username:") 
+ password=getpass("password:")
+
+
+print(user,password)
